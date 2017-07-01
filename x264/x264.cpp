f0c117b0b92df36ea8c3365793ecf4facc4636aa
JiaWeiLiou <j0939115399@gmail.com>
2017-07-01 15:07:17 +0800

    x264

--- x264/x264.cpp
+++ x264/x264.cpp
@@ -1,11 +1,21 @@
-// x264.cpp : 定義主控台應用程式的進入點。
+// x264_test.cpp : 定義主控台應用程式的進入點。
 //
 
 #include "stdafx.h"
 
+#include <iostream>  
 
-int main()
+extern "C"
 {
-    return 0;
+	#include "x264.h"  
+	#include "x264_config.h"  
 }
 
+int main(void)
+{
+	x264_param_t mParam;
+	x264_param_default(&mParam);
+	std::cout << "hello world" << std::endl;
+	system("pause");
+	return 0;
+}
